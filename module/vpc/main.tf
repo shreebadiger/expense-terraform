2464232e3f4c5b709e84a61ee1bee5225bebdc56
shree <shreesbadiger@gmail.com>
2023-12-16 21:19:26 +0530

    updated

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -34,3 +34,47 @@ resource "aws_subnet" "db" {
   cidr_block = var.db_subnet[count.index]
   tags = merge(var.tags, {Name = "db-subnet"})
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(var.tags, {Name = "public"})
+}
+
+resource "aws_route_table" "web" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(var.tags, {Name = "web"})
+}
+
+resource "aws_route_table" "app" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(var.tags, {Name = "app"})
+}
+
+resource "aws_route_table" "db" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(var.tags, {Name = "db"})
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public.*.id[count.index]
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "web" {
+  count = length(aws_subnet.web)
+  subnet_id      = aws_subnet.web.*.id[count.index]
+  route_table_id = aws_route_table.web.id
+}
+
+resource "aws_route_table_association" "app" {
+  count = length(aws_subnet.app)
+  subnet_id      = aws_subnet.app.*.id[count.index]
+  route_table_id = aws_route_table.app.id
+}
+
+resource "aws_route_table_association" "db" {
+  count = length(aws_subnet.db)
+  subnet_id      = aws_subnet.db.*.id[count.index]
+  route_table_id = aws_route_table.db.id
+}
