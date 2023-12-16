8eb54bc41ad1d6bd204edac0c889d2bf3596fa39
shree <shreesbadiger@gmail.com>
2023-12-16 21:58:21 +0530

    updated

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -47,16 +47,28 @@ resource "aws_route_table" "public" {
 resource "aws_route_table" "web" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, {Name = "web"})
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
 }
 
 resource "aws_route_table" "app" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, {Name = "app"})
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
 }
 
 resource "aws_route_table" "db" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, {Name = "db"})
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
 }
 
 resource "aws_route_table_association" "public" {
@@ -86,4 +98,13 @@ resource "aws_route_table_association" "db" {
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, {Name = "igw"})
+}
+resource "aws_eip" "ngw" {
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw.id
+  subnet_id     = aws_subnet.public.*.id[0]
+  tags = merge(var.tags, {Name = "ngw"})
 }
