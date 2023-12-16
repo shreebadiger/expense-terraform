50da8ea583ad2f0f64254b66db7b8ee2e86e760a
shree <shreesbadiger@gmail.com>
2023-12-16 21:00:28 +0530

    updated

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -3,11 +3,34 @@ resource "aws_vpc" "main" {
   tags = merge(var.tags, {Name = var.env})
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "public" {
   count = length(var.public_subnet)
   vpc_id     = aws_vpc.main.id
   availability_zone = var.azs_subnet[count.index]
   cidr_block = var.public_subnet[count.index]
   tags = merge(var.tags, {Name = "public-subnet"})
+}
 
+resource "aws_subnet" "web" {
+  count = length(var.web_subnet)
+  vpc_id     = aws_vpc.main.id
+  availability_zone = var.azs_subnet[count.index]
+  cidr_block = var.web_subnet[count.index]
+  tags = merge(var.tags, {Name = "web-subnet"})
+}
+
+resource "aws_subnet" "app" {
+  count = length(var.app_subnet)
+  vpc_id     = aws_vpc.main.id
+  availability_zone = var.azs_subnet[count.index]
+  cidr_block = var.app_subnet[count.index]
+  tags = merge(var.tags, {Name = "app-subnet"})
+}
+
+resource "aws_subnet" "db" {
+  count = length(var.db_subnet)
+  vpc_id     = aws_vpc.main.id
+  availability_zone = var.azs_subnet[count.index]
+  cidr_block = var.db_subnet[count.index]
+  tags = merge(var.tags, {Name = "db-subnet"})
 }
