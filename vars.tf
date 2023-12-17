cbb23cddea895c52267df09d5559e95c832ecd59
shree <shreesbadiger@gmail.com>
2023-12-18 01:31:55 +0530

    ugdhf

--- vars.tf
+++ vars.tf
@@ -14,7 +14,6 @@ variable "rds_allocated_storage"{}
 variable "rds_engine"{}
 variable "rds_engine_version"{}
 variable "rds_instance_class"{}
-variable "env" {}
 #variable "subnets" {}
 #variable "vpc_id" {}
 
