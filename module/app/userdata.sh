4bec500a6cd186807583ebccce6287b363e91bde
shree <shreesbadiger@gmail.com>
2023-12-21 11:32:14 +0530

    updtaed

--- module/app/userdata.sh
+++ module/app/userdata.sh
@@ -2,5 +2,5 @@
 
 yum install ansible python3.11-pip.noarch -y | tee -a /opt/userdata.log
  pip3.11 install botocore boto3 | tee -a /opt/userdata.log
-ansible-pull -i localhost, -U https://github.com/shreebadiger/ansible.git expense.yml -e role_name=${role_name} -e env=${env} | tee -a /opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/shreebadiger/ansi.git expense.yml -e role_name=${role_name} -e env=${env} | tee -a /opt/userdata.log
  
