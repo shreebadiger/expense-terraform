e4cad0523ddfd8c3dce226f364cf45b96a3e4bd7
shree <shreesbadiger@gmail.com>
2023-12-30 12:47:42 +0530

    updated

--- module/app/data.tf
+++ module/app/data.tf
@@ -1,5 +1,11 @@
+//data "aws_ami" "ami" {
+//most_recent      = true
+//name_regex       = "Centos-8-DevOps-Practice"
+//owners           = ["973714476881"]
+//}
+
 data "aws_ami" "ami" {
 most_recent      = true
-name_regex       = "Centos-8-DevOps-Practice"
-owners           = ["973714476881"]
+name_regex       = "golden-ami"
+owners           = ["751177946459"]
 }
